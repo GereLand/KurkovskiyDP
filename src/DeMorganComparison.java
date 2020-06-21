f7da00bf689fecdc09ca3e7a8d3c79562faf580d
GereLand <dan_9494@mail.ru>
2020-06-21 23:51:00 +0300

    Утро вечера мудренее...

--- src/DeMorganComparison.java
+++ src/DeMorganComparison.java
@@ -1,2 +1,16 @@
 public class DeMorganComparison {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
 }
+
+/*
+Задание 2
+Используя IntelliJ IDEA, создайте класс DeMorganComparison.
+Используя теорему Де Моргана, преобразуйте исходное выражение A | B, в эквивалентное выражение.
+ */
--- src/LogicOperations.java
+++ src/LogicOperations.java
@@ -1,2 +1,19 @@
 public class LogicOperations {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        int z = 15;
+        x += y >> x++ * z; //
+        z = ++x & y * 5; //
+        y /= x + 5 | z; //
+        z = x++ & y * 5; //
+        x = y << x++ ^ z; //
+    }
 }
+
+/*
+Задание 5
+Используя IntelliJ IDEA, создайте класс LogicOperations.
+Имеется 3 переменные типа int x = 5, y = 10, и z = 15;
+Выполните и рассчитайте результат следующих операций для этих переменных:
+ */
