58985408b117434f369d94be9fbfd1c237b60b76
GereLand <Dan_9494@mail.ru>
2020-06-29 00:04:48 +0300

    Выполнение задания №2 для дз от 24.06

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Circle {
     /**
      * Задание 2
@@ -6,6 +8,11 @@ public class Circle {
      * Используя формулу πR 2 , вычислите площадь круга и выведите результат на экран.
      */
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите значение радиуса: ");
+        double r = scan.nextInt();
+        final double PI = 3.1415926535;
+        double S = PI * (r * r);
+        System.out.println(S);
     }
 }
