d7c49ebbadd8b400e1f67e1671f9c5f58fde2c8b
GereLand <dan_9494@mail.ru>
2020-06-28 21:58:30 +0300

    Создал классы для выполнения дз от 24.06. Маленькое начало - тоже начало.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /**
+     * Задание 4
+     *
+     * Проверьте, можно ли создать переменные со следующими именами:
+     * uberflu? , _Identifier , \u006fIdentifier , &myVar , myVariab1le
+     */
+    public static void main(String[] args) {
+
+    }
+}
