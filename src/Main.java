d6aa0cdab74ffda03459a571d4b842b12b8ec3fb
GereLand <dan_9494@mail.ru>
2020-06-28 23:55:43 +0300

    Выполнение задания №4 для дз от 24.06

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,15 @@ public class Main {
      * uberflu? , _Identifier , \u006fIdentifier , &myVar , myVariab1le
      */
     public static void main(String[] args) {
-
+//    int uberflu? = 1; // //Невозможно создать данную переменную - компилятор ругается
+    int _Identifier = 2; //Возможно создать данную переменную
+    int \u006fIdentifier = 3; //Создать переменную возможно и даже можно вывести её значение, но программа ругается на такое название. Судя по всему лучше такое не использовать.
+//    int &myVar = 4; //Невозможно создать данную переменную - компилятор ругается
+    int myVariab1le = 5; //Возможно создать данную переменную
+//        System.out.println(uberflu?);
+        System.out.println(_Identifier);
+        System.out.println(\u006fIdentifier);
+//        System.out.println(&myVar);
+        System.out.print(myVariab1le);
     }
 }
