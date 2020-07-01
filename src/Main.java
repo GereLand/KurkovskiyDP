c2a4ac1a3a4dc564075c5158c2aac4d89caed404
GereLand <dan_9494@mail.ru>
2020-07-01 18:24:35 +0300

    Коррекция задания №4 для дз от 24.06 - бесит меня, что класс красным светится из-за некорректного названия, хотя программа схавывает и всё работает.

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,19 @@ public class Main {
      */
     public static void main(String[] args) {
 //    int uberflu? = 1; // //Невозможно создать данную переменную - компилятор ругается
+
     int _Identifier = 2; //Возможно создать данную переменную
-    int \u006fIdentifier = 3; //Создать переменную возможно и даже можно вывести её значение, но программа ругается на такое название. Судя по всему лучше такое не использовать.
+
+//    int \u006fIdentifier = 3; //Создать переменную возможно и даже можно вывести её значение,
+        // но программа ругается на такое название. Судя по всему лучше такое не использовать.
+
 //    int &myVar = 4; //Невозможно создать данную переменную - компилятор ругается
+
     int myVariab1le = 5; //Возможно создать данную переменную
+
 //        System.out.println(uberflu?);
         System.out.println(_Identifier);
-        System.out.println(\u006fIdentifier);
+//        System.out.println(\u006fIdentifier);
 //        System.out.println(&myVar);
         System.out.print(myVariab1le);
     }
