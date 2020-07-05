034d793ffb90b23851292b3fbaea4c05ebb0b66b
GereLand <dan_9494@mail.ru>
2020-07-05 14:29:03 +0300

    Решение задания №2 для выполнения дз от 29.06

--- src/Homework3/Calculator.java
+++ src/Homework3/Calculator.java
@@ -1,9 +1,11 @@
 package Homework3;
 
+import java.util.Scanner;
+
 public class Calculator {
     /**
      * Задание 2
-     *
+     * <p>
      * Напишите программу – консольный калькулятор. Создайте две переменные с
      * именами operand1 и operand2.
      * Задайте переменным некоторые произвольные значения. Предложите
@@ -16,7 +18,63 @@ public class Calculator {
      * отмените выполнение арифметической операции и уведомите об ошибке
      * пользователя .
      */
+    // Вариант выполнения задания согласно условиям.
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        int operand1 = 10;
+        int operand2 = 5;
+        System.out.print("Введите знак арифметической операции: ");
+        String sign = scan.next();
+        switch (sign) {
+            case "+":
+                System.out.print("Результат вычисления = " + (operand1 + operand2));
+                break;
+            case "-":
+                System.out.print("Результат вычисления = " + (operand1 - operand2));
+                break;
+            case "*":
+                System.out.print("Результат вычисления = " + (operand1 * operand2));
+                break;
+            case "/":
+            // Не нашёл другого способа сделать исключение в switch, поэтому использовал if
+                if (operand2 != 0)
+                System.out.print("Результат вычисления = " + (operand1 / operand2));
+                else System.out.println("Невозможно произвести операцию деления на ноль!");
+                break;
+            default:
+                System.out.print("Введено некорректное значение!");
+                break;
+        }
     }
+    // Вариант выполнения задания с использованием ручного введения операндов в консоль.
+    /*
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите значение переменной А: ");
+        int operand1 = scan.nextInt();
+        System.out.print("Введите знак арифметической операции: ");
+        String sign = scan.next();
+        System.out.print("Введите значение переменной В: ");
+        int operand2 = scan.nextInt();
+        switch (sign) {
+            case "+":
+                System.out.print("Результат вычисления = " + (operand1 + operand2));
+                break;
+            case "-":
+                System.out.print("Результат вычисления = " + (operand1 - operand2));
+                break;
+            case "*":
+                System.out.print("Результат вычисления = " + (operand1 * operand2));
+                break;
+            case "/":
+                if (operand2 != 0)
+                    System.out.print("Результат вычисления = " + (operand1 / operand2));
+                else System.out.println("Невозможно произвести операцию деления на ноль!");
+                break;
+            default:
+                System.out.print("Введено некорректное значение!");
+                break;
+        }
+    }
+     */
 }
