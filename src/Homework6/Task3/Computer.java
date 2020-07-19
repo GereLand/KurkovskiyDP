f5be2877c76ec97e2f41afaa3a4922b60e2e9760
GereLand <dan_9494@mail.ru>
2020-07-19 14:16:27 +0300

    Создал модули с классами для выполнения домашних заданий 5 и 6, пока без решения заданий.

---
+++ src/Homework6/Task3/Computer.java
@@ -0,0 +1,14 @@
+package Homework6.Task3;
+
+public class Computer {
+    /**
+     * Задание 3
+     * Используя IDEA, создайте проект с пакетом.
+     * Создать класс Computer, создать массив объектов Computers размером 5.
+     * Далее руками создать 5 экземпляров этого класса и записать в компьютер
+     * (используя loop)
+     */
+    public static void main(String[] args) {
+
+    }
+}
