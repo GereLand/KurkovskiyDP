832c7f52a8663bc8de3db9ad76c5ecb633f23d58
GereLand <dan_9494@mail.ru>
2020-07-29 00:33:36 +0300

    Выполнил задание 2 для дз №5

--- src/Homework5/ReversedArray.java
+++ src/Homework5/ReversedArray.java
@@ -1,9 +1,12 @@
 package Homework5;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReversedArray {
     /**
      * Задание 2
-     *
+     * <p>
      * Требуется:
      * 1) Создать метод myReverse(int [] array), который принимает в качестве аргумента массив
      * целочисленных элементов и возвращает инвертированный массив (элементы массива в обратном
@@ -17,6 +20,52 @@ public class ReversedArray {
      * элементы, которые не были скопированы из исходного массива.
      */
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Задайте размер массива: ");
+        int N = scan.nextInt();
+        int[] array = new int[N];
+        int x = 1;
+        for (int y = 0; y < N; ) {
+            System.out.print("Значение в массиве в ячейке №" + x + " = ");
+            array[y] = scan.nextInt();
+            x++;
+            y++;
+        }
+        myReverse(array);
+        System.out.print("Задайте значение index: ");
+        int index = scan.nextInt();
+        System.out.print("Задайте значение count: ");
+        int count = scan.nextInt();
+        subArray(array, index, count);
+    }
 
+    public static int[] myReverse(int[] array) {
+        System.out.println("Первоначальный массив : " + Arrays.toString(array));
+        if (array == null || array.length <= 1) {
+            System.out.println("Заданный массив невозможно перевернуть!");
+            return array;
+        }
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+        System.out.println("Перевёрнутый массив : " + Arrays.toString(array));
+        return array;
+    }
+
+    public static int[] subArray(int[] array, int index, int count) {
+        System.out.print("Видоизменённый массив: [");
+        for (int i = index; i < (index + count); i++) {
+            // if я сделал заковыристым просто для единообразия вывода с предыдущими массивами. Люблю когда всё красиво.
+            if (i < array.length)
+                System.out.print(array[i] + ", ");
+            else if (i != (index + count -1))
+                System.out.print(1 + ", ");
+            else
+                System.out.print(1);
+        }
+        System.out.print("]");
+        return array;
     }
 }
