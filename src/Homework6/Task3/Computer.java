c20b2f6ede2ee9b06fc641a558a3a440c73f55bf
GereLand <Dan_9494@mail.ru>
2020-08-08 16:11:04 +0300

    Попытка выполнения задание 3 для дз №6

--- src/Homework6/Task3/Computer.java
+++ src/Homework6/Task3/Computer.java
@@ -1,5 +1,7 @@
 package Homework6.Task3;
 
+import java.util.Scanner;
+
 public class Computer {
     /**
      * Задание 3
@@ -8,8 +10,56 @@ public class Computer {
      * Далее руками создать 5 экземпляров этого класса и записать в компьютер
      * (используя loop)
      */
-    // Пока не до конца понял задание, уточню на занятии.
+    // Предварительно сделал так, оно работает, но я бы хотел сделать через экземпляры класса, но пока не понял, как это
+    // работает с массивами.
+
+    private static String[] computers = new String[5];
+
     public static void main(String[] args) {
-        String[] Computers = new String[5];
+//
+//        Computer computer1 = new Computer();
+////        Computer.computers[0] = computer1;
+//        Computer computer2 = new Computer();
+//        Computer computer3 = new Computer();
+//        Computer computer4 = new Computer();
+//        Computer computer5 = new Computer();
+
+        Scanner scan = new Scanner(System.in);
+
+        for (int index = 0; index < computers.length; index++) {
+            switch (index) {
+                case 0:
+                    System.out.print("Введите данные компьютера №1: ");
+                    computers[0] = scan.nextLine();
+                    break;
+                case 1:
+                    System.out.print("Введите данные компьютера №2: ");
+                    computers[1] = scan.nextLine();
+                    break;
+                case 2:
+                    System.out.print("Введите данные компьютера №3: ");
+                    computers[2] = scan.nextLine();
+                    break;
+                case 3:
+                    System.out.print("Введите данные компьютера №4: ");
+                    computers[3] = scan.nextLine();
+                    break;
+                case 4:
+                    System.out.print("Введите данные компьютера №5: ");
+                    computers[4] = scan.nextLine();
+                    break;
+            }
+        }
+
+        Show(computers);
+    }
+
+    private static void Show(String[] computers) {
+
+        System.out.println("Данные всех компьютеров: ");
+        for (String computer : computers) {
+            System.out.println(computer);
+        }
+
     }
 }
